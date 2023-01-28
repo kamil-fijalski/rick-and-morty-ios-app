ec0be59b1e97b050409e17dbfe7ed63250671743
kamil-fijalski <kamil.fijalski@outlook.com>
2023-01-29 00:40:14 +0100

    add documentation init and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Kamil Fijalski on 29/01/2023.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
---
+++ RickAndMorty/APIClient/RMService.swift
@@ -0,0 +1,25 @@
+//
+//  RMService.swift
+//  RickAndMorty
+//
+//  Created by Kamil Fijalski on 29/01/2023.
+//
+
+import Foundation
+
+/// Primary API service object to get Rick and Morty data
+final class RMService {
+    /// Shared singleton instance
+    static let shared = RMService()
+    
+    /// Privatized costructor
+    private init() {}
+    
+    /// Send Rick and Morty API call
+    /// - Parameters:
+    ///   - request: Request instance
+    ///   - completion: Callback with data or error
+    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+        
+    }
+}
